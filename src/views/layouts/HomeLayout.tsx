d55dc56aee84bfc076d1155f14a76c3a7c2fc08c
Andrés Meléndez <codeslator@gmail.com>
2022-08-14 12:33:40 -0400

    Creating Footer and adding to HomeLayout;

--- src/views/layouts/HomeLayout.tsx
+++ src/views/layouts/HomeLayout.tsx
@@ -1,14 +1,16 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/home/Footer'
 import NavBar from '../components/home/NavBar'
 
 const HomeLayout: FC = () => {
   return (
-    <div className="h-screen bg-gray-800">
+    <div className="h-screen bg-gray-800 flex flex-col justify-between">
       <NavBar />
       <div className="">
         <Outlet />
       </div>
+      <Footer />
     </div>
   )
 }
