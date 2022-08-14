f8228e47bf62c4dff9e58dd67600dd957abe4be4
Andrés Meléndez <codeslator@gmail.com>
2022-08-14 15:22:12 -0400

    Customizing TodoItem & TodoList components.

--- src/views/components/todos/TodoList.tsx
+++ src/views/components/todos/TodoList.tsx
@@ -1,8 +1,30 @@
-import { FC } from 'react'
+import { FC } from 'react';
+import TodoItem from './TodoItem';
 
 const TodoList: FC = () => {
   return (
-    <div>TodoList</div>
+    <div className="p-4 w-full bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-between items-center mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Todos</h5>
+        {/* <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+          View all
+        </a> */}
+      </div>
+      <div className="flow-root ">
+        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700 h-[400px] overflow-y-auto">
+          <TodoItem title="Hello world" completed={false} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+          <TodoItem title="Hello world" completed={true} userId={1} />
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- src/views/pages/TodosPage.tsx
+++ src/views/pages/TodosPage.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react'
+import TodoList from '../components/todos/TodoList';
 
 const TodosPage: FC = () => {
   return (
-    <div>TodosPage</div>
+    <div className="mx-auto w-11/12">
+      <p className="text-white text-3xl my-5">ToDos List</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <TodoList />
+      </div>
+
+    </div>
   );
 };
 
