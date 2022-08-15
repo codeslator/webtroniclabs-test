ff360ecf5aaa2b63493f31465ffb9c895782d522
Andrés Meléndez <codeslator@gmail.com>
2022-08-14 22:35:43 -0400

    Adding hero card and content in HomePage; Creating new Navbar in AuthLayout;

--- src/views/pages/HomePage.tsx
+++ src/views/pages/HomePage.tsx
@@ -3,7 +3,32 @@ import { FC } from 'react';
 const HomePage: FC = () => {
   return (
     <div className="mx-auto w-10/12">
-      
+      <div className="p-4 w-full text-left bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
+        <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.</p>
+
+        <h2 className="mb-5 text-2xl text-left font-extrabold tracking-tight text-gray-900 dark:text-white">We invest in the world’s potential</h2>
+        <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
+          <li className="flex space-x-2">
+            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+            <span className="font-light leading-tight">Dynamic reports and dashboards</span>
+          </li>
+          <li className="flex space-x-2">
+            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+            <span className="font-light leading-tight">Templates for everyone</span>
+          </li>
+          <li className="flex space-x-2">
+            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+            <span className="font-light leading-tight">Development workflow</span>
+          </li>
+          <li className="flex space-x-2">
+            <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+            <span className="font-light leading-tight">Limitless business automation</span>
+          </li>
+        </ul>
+
+      </div>
+
     </div>
   );
 };
